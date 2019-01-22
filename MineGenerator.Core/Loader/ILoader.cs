dbc3ffd480709d624690f4a92386d183c459c6bc
experdot <experdot@foxmail.com>
2019-01-22 23:11:50 +0800

    Storage: Add package model
    consist of Id, Blocks and ExtendProperties

---
+++ MineGenerator.Core/Loader/ILoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineGenerator.Core.Loader
+{
+    /// <summary>
+    /// Represents a block loader.
+    /// </summary>
+    interface ILoader
+    {
+        IEnumerable<IBlock> Load();
+    }
+}
---
+++ MineGenerator.Core/Storage/Package.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineGenerator.Core.Storage
+{
+    public class Package
+    {
+        public Guid Id { get; set; }
+        public IEnumerable<IBlock> Blocks { get; set; }
+        public Dictionary<string, object> ExtendProperties { get; set; }
+    }
+}
