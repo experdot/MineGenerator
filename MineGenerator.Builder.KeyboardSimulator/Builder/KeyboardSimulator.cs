5190f50584b0d1dd2b8d296d93df4304beb296ca
experdot <experdot@foxmail.com>
2019-02-23 23:58:54 +0800

    Builder: Generate armors army
    summon entites in 100x100 region

--- MineGenerator.Builder.KeyboardSimulator/Builder/KeyboardSimulator.cs
+++ MineGenerator.Builder.KeyboardSimulator/Builder/KeyboardSimulator.cs
@@ -12,13 +12,24 @@ namespace MineGenerator.Builder
             System.Threading.Thread.Sleep(8000);// Delay 3000ms
             foreach (var item in regions)
             {
-                BuildSingleBlock(item);
+                var command = GenerateSummonCommand(item);
+                ExecuteCommand(command);
             }
+
+            ExcuteArmorCommands();
         }
 
-        public void BuildSingleBlock(IRegion region)
+        private void ExcuteArmorCommands()
         {
-            var command = GenerateCommand(region);
+            ExecuteCommand("/replaceitem entity @e[type=armor_stand] slot.weapon.mainhand 0 golden_sword");
+            ExecuteCommand("/replaceitem entity @e[type=armor_stand] slot.armor.head 0 skull");
+            ExecuteCommand("/replaceitem entity @e[type=armor_stand] slot.armor.chest 0 iron_chestplate");
+            ExecuteCommand("/replaceitem entity @e[type=armor_stand] slot.armor.legs 0 iron_leggings");
+            ExecuteCommand("/replaceitem entity @e[type=armor_stand] slot.armor.feet 0 iron_boots");
+        }
+
+        private void ExecuteCommand(string command)
+        {
             VirtualKeyboard.SendKey(VirtualKeys.VK_T);
             System.Threading.Thread.Sleep(200);
             VirtualKeyboard.SendKey(VirtualKeys.VK_RETURN);
@@ -30,7 +41,15 @@ namespace MineGenerator.Builder
             System.Threading.Thread.Sleep(100);
         }
 
-        public string GenerateCommand(IRegion region)
+        private string GenerateSummonCommand(IRegion region)
+        {
+            var sx = Math.Round(region.Start.X);
+            var sy = Math.Round(region.Start.Y);
+            var sz = Math.Round(region.Start.Z);
+            return $"/summon armor_stand ~{sx} ~{sy} ~{sz}";
+        }
+
+        private string GenerateFillCommand(IRegion region)
         {
             var sx = Math.Round(region.Start.X);
             var sy = Math.Round(region.Start.Y);
@@ -38,8 +57,8 @@ namespace MineGenerator.Builder
             var ex = Math.Round(region.End.X);
             var ey = Math.Round(region.End.Y);
             var ez = Math.Round(region.End.Z);
-
             return $"/fill ~{sx} ~{sy} ~{sz} ~{ex} ~{ey} ~{ez} concrete";
         }
+
     }
 }
--- MineGenerator.Loader.Geometry/Loader/GeometryLoader.cs
+++ MineGenerator.Loader.Geometry/Loader/GeometryLoader.cs
@@ -10,16 +10,40 @@ namespace MineGenerator.Loader.Geometry
     {
         public IEnumerable<IRegion> Load()
         {
+            return GenerateRectangle();
+        }
+
+        private IEnumerable<IRegion> GenerateCircle()
+        {
             var radius = 50;
             var regions = new List<IRegion>();
             var line = new Vector2(0, radius);
             var circumference = Math.Ceiling(radius * 2 * Math.PI);
+
             for (int i = 0; i < circumference; i += 5)
             {
                 var rotation = (float)(Math.PI * 2 * i / circumference);
                 var current = Vector2.Transform(line, Matrix3x2.CreateRotation(rotation));
-                regions.Add(new Region(new Vector3(current.X, current.Y, 10), new Vector3(0, 0, 10)));
+                regions.Add(new Region(new Vector3(current.X, current.Y, 10)));
             }
+            return regions;
+        }
+
+        private IEnumerable<IRegion> GenerateRectangle()
+        {
+            var regions = new List<IRegion>();
+            var random = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    var offsetX = random.Next(5);
+                    var offsetY = random.Next(5);
+                    regions.Add(new Region(new Vector3(i * 5 + offsetX, -1, j * 5 + offsetY)));
+                }
+            }
+
             return regions;
         }
     }
